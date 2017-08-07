da2ddb23cbfce49d5c4ce7ee94acc882532e0e57
Favina <favinaa@gmail.com>
2017-08-07 18:42:16 +0100

    Working Version

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express')
 const app = express()
 const request = require('request');
-var auth = require('auth');
 
+
+var passport = require('passport');
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+        clientID: "195086391779-d84p1svvvdvrvp3fe8uc6rsm0eerlrue.apps.googleusercontent.com",
+        clientSecret: "uLyfQ6R1MiJORHv9gRGct2nG",
+        callbackURL: "http://localhost:3000"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
+    }
+));
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
 app.get('/auth/google',
-    auth.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -17,7 +36,12 @@ app.get('/auth/google',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/google/callback',
-    auth.authenticate('google', { failureRedirect: '/login' }),
+    passport.authenticate('google', { failureRedirect: '/login' }),
     function(req, res) {
-        res.redirect('/');
+        res.redirect('https://www.google.co.uk/');
     });
+const server = app.listen(process.env.PORT || 3000, function () {
+
+    const port = server.address().port ;
+    console.log('Example app listening on port ' + port);
+})
