c8c6f6e553a979077864af2ae5fa499f8a949f50
Favina <favinaa@gmail.com>
2017-08-10 21:23:41 +0100

    Working Version

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const app = new ApiAiApp({ request, response });
+const app = new ApiAiApp({request: request, response: response});
 const WELCOME_INTENT = 'input.welcome';
-const SIGN_IN = 'sign.in';
+const NUMBER_INTENT = 'input.number';
 
 function welcomeIntent (app) {
-    app.askForSignIn();
+    app.ask('Welcome to action snippets! Say a number.');
 }
 
-function signIn (app) {
-    if (app.getSignInStatus() === app.SignInStatus.OK) {
-        let accessToken = app.getUser().accessToken;
-        app.ask('Great, thanks for signing in!');
-    } else {
-        app.ask('I won\'t be able to save your data, but let\'s continue!');
-    }
+function numberIntent (app) {
+    const number = app.getArgument(NUMBER_ARGUMENT);
+    app.tell('You said ' + number);
 }
 
 const actionMap = new Map();
 actionMap.set(WELCOME_INTENT, welcomeIntent);
-actionMap.set(SIGN_IN, signIn);
+actionMap.set(NUMBER_INTENT, numberIntent);
 app.handleRequest(actionMap);
