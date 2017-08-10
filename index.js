cede9360077377f58e8da7d0407354cff538a9c3
Favina <favinaa@gmail.com>
2017-08-10 17:44:05 +0100

    Working Version

--- index.js
+++ index.js
@@ -1,76 +1,21 @@
-const express = require('express')
-const app = express()
-const request = require('request');
+const app = new ApiAiApp({ request, response });
+const WELCOME_INTENT = 'input.welcome';
+const SIGN_IN = 'sign.in';
 
-var google = require('googleapis');
-
-app.post('/calendar', function (req, res) {
-
-    var calendarId;
-    if (req.params.calendarId){
-        calendarId = req.params.calendarId;
-
-    }
-    else
-    {
-        calendarId = 'en.uk#holiday@group.v.calendar.google.com';
-    }
-
-    var cal = 'https://www.googleapis.com/calendar/v3/calendars/'+ calendarId +'/events';
-
-
-    request(cal, function (error, response) {
-        var events = {}
-        if (error){
-            response.status(500);
-        }
-        else {
-
-            events.type = 0;
-            events.speech = "your events"+ listEvents();
-            events.displayText = events.speech;
-            events.data = {};
-            events.contextOut = [ ];
-            events.source = " Our weather App";
-
-            res.json(events);
-
-        }
-    });
-
-
-});
-
-function listEvents() {
-    var calendar = google.calendar('v3');
-    calendar.events.list({
-        auth: auth,
-        calendarId: 'primary',
-        timeMin: (new Date()).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime'
-    }, function(err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-        }
-        var events = response.items;
-        if (events.length == 0) {
-            console.log('No upcoming events found.');
-        } else {
-            console.log('Upcoming 10 events:');
-            for (var i = 0; i < events.length; i++) {
-                var event = events[i];
-                var start = event.start.dateTime || event.start.date;
-                console.log('%s - %s', start, event.summary);
-            }
-        }
-    });
+function welcomeIntent (app) {
+    app.askForSignIn();
 }
 
-const server = app.listen(process.env.PORT || 3000, function () {
+function signIn (app) {
+    if (app.getSignInStatus() === app.SignInStatus.OK) {
+        let accessToken = app.getUser().accessToken;
+        app.ask('Great, thanks for signing in!');
+    } else {
+        app.ask('I won\'t be able to save your data, but let\'s continue!');
+    }
+}
 
-    const port = server.address().port ;
-    console.log('Example app listening on port ' + port);
-})
+const actionMap = new Map();
+actionMap.set(WELCOME_INTENT, welcomeIntent);
+actionMap.set(SIGN_IN, signIn);
+app.handleRequest(actionMap);
