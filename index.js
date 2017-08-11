2fec1af9977765eee0dc973c2fc7699f6f34aa92
Favina <favinaa@gmail.com>
2017-08-11 10:02:15 +0100

    Working version

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
-const app = new ApiAiApp({request: request, response: response});
-const WELCOME_INTENT = 'input.welcome';
-const NUMBER_INTENT = 'input.number';
+const express = require('express')
+const app = express()
+const request = require('request');
 
-function welcomeIntent (app) {
-    app.ask('Welcome to action snippets! Say a number.');
+var google = require('googleapis');
+
+app.post('/calendar', function (req, res) {
+
+    var calendarId;
+    if (req.params.calendarId){
+        calendarId = req.params.calendarId;
+
+    }
+    else
+    {
+        calendarId = 'en.uk#holiday@group.v.calendar.google.com';
+    }
+
+    var cal = 'https://www.googleapis.com/calendar/v3/calendars/'+ calendarId +'/events';
+
+
+    request(cal, function (error, response) {
+        var events = {}
+        if (error){
+            response.status(500);
+        }
+        else {
+
+            events.type = 0;
+            events.speech = "your events";
+            events.displayText = events.speech;
+            events.data = {};
+            events.contextOut = [ ];
+            events.source = " Our weather App";
+
+            res.json(events);
+
+        }
+    });
+
+
+});
+
+function listEvents() {
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+        auth: auth,
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var events = response.items;
+        if (events.length == 0) {
+            console.log('No upcoming events found.');
+        } else {
+            console.log('Upcoming 10 events:');
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i];
+                var start = event.start.dateTime || event.start.date;
+                console.log('%s - %s', start, event.summary);
+            }
+        }
+    });
 }
 
-function numberIntent (app) {
-    const number = app.getArgument(NUMBER_ARGUMENT);
-    app.tell('You said ' + number);
-}
+const server = app.listen(process.env.PORT || 3000, function () {
 
-const actionMap = new Map();
-actionMap.set(WELCOME_INTENT, welcomeIntent);
-actionMap.set(NUMBER_INTENT, numberIntent);
-app.handleRequest(actionMap);
+    const port = server.address().port ;
+    console.log('Example app listening on port ' + port);
+})
