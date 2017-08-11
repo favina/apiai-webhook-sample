ca67964abee8418cdc138e9e90ad032088509505
Favina <favinaa@gmail.com>
2017-08-11 11:12:48 +0100

    Working version

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.post('/calendar', function (req, res) {
         }
         else {
 
-           
+
 
             events.type = 0;
-            events.speech = "your events";
+            events.speech = "your events" + events.listEvents();
             events.displayText = events.speech;
             events.data = {};
             events.contextOut = [ ];
@@ -43,36 +43,37 @@ app.post('/calendar', function (req, res) {
         }
     });
 
+    function listEvents() {
+        var calendar = google.calendar('v3');
+        calendar.events.list({
+            auth: auth,
+            calendarId: 'primary',
+            timeMin: (new Date()).toISOString(),
+            maxResults: 10,
+            singleEvents: true,
+            orderBy: 'startTime'
+        }, function(err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return;
+            }
+            var events = response.items;
+            if (events.length == 0) {
+                console.log('No upcoming events found.');
+            } else {
+                console.log('Upcoming 10 events:');
+                for (var i = 0; i < events.length; i++) {
+                    var event = events[i];
+                    var start = event.start.dateTime || event.start.date;
+                    console.log('%s - %s', start, event.summary);
+                }
+            }
+        });
+    }
 
 });
 
-function listEvents() {
-    var calendar = google.calendar('v3');
-    calendar.events.list({
-        auth: auth,
-        calendarId: 'primary',
-        timeMin: (new Date()).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime'
-    }, function(err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
-        }
-        var events = response.items;
-        if (events.length == 0) {
-            console.log('No upcoming events found.');
-        } else {
-            console.log('Upcoming 10 events:');
-            for (var i = 0; i < events.length; i++) {
-                var event = events[i];
-                var start = event.start.dateTime || event.start.date;
-                console.log('%s - %s', start, event.summary);
-            }
-        }
-    });
-}
+
 
 const server = app.listen(process.env.PORT || 3000, function () {
 
