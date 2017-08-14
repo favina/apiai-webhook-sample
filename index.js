73cdc9f33df0b443d91245d2a28461bb44581df1
Favina <favinaa@gmail.com>
2017-08-14 14:59:28 +0100

    Working Version

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const app = new ApiAiApp({ request, response });
-const WELCOME_INTENT = 'input.welcome';
-const DATETIME = 'datetime';
-
-function welcomeIntent (app) {
-    app.askForDateTime('When do you want to come in?',
-        'Which date works best for you?',
-        'What time of day works best for you?');
-}
-
-function datetime (app) {
-    app.tell({speech: 'Great see you at your appointment!',
-        displayText: 'Great, we will see you on '
-        + app.getDateTime().date.month
-        + '/' + app.getDateTime().date.day
-        + ' at ' + app.getDateTime().time.hours
-        + (app.getDateTime().time.minutes || '')});
-}
-
-const actionMap = new Map();
-actionMap.set(WELCOME_INTENT, welcomeIntent);
-actionMap.set(DATETIME, datetime);
-app.handleRequest(actionMap);
+'use strict';
 
 
+const request = require('request');
+
+
+process.env.DEBUG = 'actions-on-google:*';
+
+
+var signin;
+
+// initialize the express application
+var express = require("express"),
+    app = express();
+
+// initialize the Fitbit API client
+var FitbitApiClient = require("fitbit-node"),
+    client = new FitbitApiClient("228PW6", "fa58721e03216857b04f08e1e46ecfb8");
+
+app.post('/check', function (req, res) {
+
+    var signin;
+    if (req.params.signin) {
+
+        // redirect the user to the Fitbit authorization page
+        app.get("/authorize", function (req, res) {
+            // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
+            res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://oauth-redirect.googleusercontent.com/r/application-757db'));
+        });
+
+        // handle the callback from the Fitbit authorization flow
+        app.get("https://oauth-redirect.googleusercontent.com/r/application-757db/callback", function (req, res) {
+            // exchange the authorization code we just received for an access token
+            client.getAccessToken(req.query.code, 'https://oauth-redirect.googleusercontent.com/r/application-757db').then(function (result) {
+                // use the access token to fetch the user's profile information
+                client.get("/profile.json", result.access_token).then(function (results) {
+                    res.send(results[0]);
+                });
+            }).catch(function (error) {
+                res.send(error);
+            });
+        });
+    }
 
 
 
+    }
 
 
+
+// launch the server
+app.listen(3000);
